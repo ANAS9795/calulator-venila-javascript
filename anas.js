8a4a68b4df1297a1b91c9f2047d2500f8f1067ea
ANAS9795 <mohdanas9272@gmail.com>
2023-02-12 16:45:02 +0530

    Created ui and functionality for calculator

---
+++ anas.js
@@ -0,0 +1,27 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  console.log(screen);
+  let equal = document.querySelector(".btn-equal");
+  let clear = document.querySelector(".btn-clear");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      if (typeof value !== "undefined") {
+        screen.value += value;
+      }
+    });
+  });
+  clear.addEventListener("click", function () {
+    screen.value = "";
+  });
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "Please Enter value";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+})();
