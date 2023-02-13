92e2cb0c631c2fb43fbdcef3dc732d277f5a176d
ANAS9795 <mohdanas9272@gmail.com>
2023-02-13 19:37:10 +0530

    change style and add button

--- Index.js
+++ Index.js
@@ -1,8 +1,15 @@
 (function () {
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
-  let equal = document.querySelector(".btn-equal");
+  let equal = document.querySelector(".equal");
+  let removeOneCharacterBtn = document.querySelector(".btn-remove");
   let clear = document.querySelector(".btn-clear");
+
+  removeOneCharacterBtn.addEventListener("click", function (e) {
+    screen.value = screen.value.length
+      ? screen.value.substring(0, screen.value.length - 1)
+      : "";
+  });
 
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
@@ -17,10 +24,19 @@
   });
   equal.addEventListener("click", function (e) {
     if (screen.value === "") {
-      screen.value = "Please Enter value";
+      screen.value = "Enter value";
     } else {
       let answer = eval(screen.value);
       screen.value = answer;
     }
   });
 })();
+let icon01 = document.getElementById("icon01");
+icon01.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon01.src = "img-icon/sun.png";
+  } else {
+    icon01.src = "img-icon/moon.png";
+  }
+};
